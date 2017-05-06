a0b65702baacaa1a925d19ae8f72ec50a1373e47
Mike Lovely <mike-lovely@hotmail.co.uk>
2017-05-06 22:31:11 +0100

    Revert

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -6,11 +6,7 @@
 
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:800,700,500,400">
         <link href='https://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
-        @if (Request::secure())
-            <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
-        @else
-            <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        @endif
+        <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
     </head>
     <body>
         @yield('header')
