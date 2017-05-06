e02e92ef6ccece38316ae7f721a687f952a8eb8e
Mike Lovely <mike-lovely@hotmail.co.uk>
2017-05-06 22:20:30 +0100

    Decide asset URL when using HTTP or HTTPS

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -3,10 +3,14 @@
     <head>
         <meta charset="UTF-8">
         <title>Blog</title>
-    
+
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway:800,700,500,400">
         <link href='https://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        @if (Request::secure())
+            <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
+        @else
+            <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        @endif
     </head>
     <body>
         @yield('header')
